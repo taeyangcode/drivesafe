6fdd1c77a3ed049bc75c6966aa8aff9c82cd37dc
Jason <Jasonel2@uci.edu>
2024-01-26 22:45:30 -0800

    created server fastapi

---
+++ api/server.py
@@ -0,0 +1,20 @@
+from typing import Union
+from fastapi import FastAPI
+from main import *
+app = FastAPI()
+
+
+@app.get("/crashes")
+def read_root():
+    return {
+        "hello":"test"
+    }
+
+@app.get("/get_nearby_accidents/")
+async def get_nearby_accidents(user_lat: float, user_lng: float):
+    temp_radius = 5
+    return points_within_radius((user_lat, user_lng), temp_radius)
+
+if __name__ == "__main__":
+    pass
+
