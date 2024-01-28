21feb100d432a7144157b49b8e4629445d821ab3
Kateyn Wang <katelyn.j.wang@gmail.com>
2024-01-27 20:35:25 -0800

    changed radius to bounds; added city/county/state

--- api/main.py
+++ api/main.py
@@ -4,9 +4,16 @@ import pandas as pd
 # data = pd.read_csv("US_Accidents_March23_sampled_500k.csv")[['ID','Severity','Start_Lat','Start_Lng']]
 data = pd.read_csv("US_Accidents_March23.csv")[['ID','Severity','Start_Lat','Start_Lng']]
 
-# print(data) 
+# load data from csv files for city, county, and state counts
+city_data = pd.read_csv("cities_data.csv")
+county_data = pd.read_csv("counties_data.csv")
+state_data = pd.read_csv("states_data.csv")
+
 
 def points_within_radius(data, center, radius):
+    '''
+    Return individual car crashes that occur within the given radius from the center
+    '''
     result = []
     filtered_data = data[data['Start_Lat']>= center[0] -radius/69]
     filtered_data = filtered_data[filtered_data['Start_Lat']<= center[0] +radius/69]
@@ -15,6 +22,33 @@ def points_within_radius(data, center, radius):
 
     for index,row in filtered_data.iterrows():
         result.append({"ID":row['ID'],"Severity":row['Severity'],"Latitude":row['Start_Lat'],"Longitude":row['Start_Lng']})
+    return result
+
+
+def points_within_bounds(data, north: float, south: float, east: float, west: float):
+    '''
+    Return individual car crashes that occur within the rectangular bounds provided
+    '''
+    result = []
+    filtered_data = data[data['Start_Lat'] >= west]
+    filtered_data = filtered_data[filtered_data['Start_Lat'] <= east]
+    filtered_data = filtered_data[filtered_data['Start_Lng'] >= south]
+    filtered_data = filtered_data[filtered_data['Start_Lng'] <= north]
+
+    for index,row in filtered_data.iterrows():
+        result.append({"ID":row['ID'],"Severity":row['Severity'],"Latitude":row['Start_Lat'],"Longitude":row['Start_Lng']})
+    return result
+
+
+def grouped_crashes_in_bounds(data, north, south, east, west):
+    result = []
+    filtered_data = data[data['Start_Lat'] >= west]
+    filtered_data = filtered_data[filtered_data['Start_Lat'] <= east]
+    filtered_data = filtered_data[filtered_data['Start_Lng'] >= south]
+    filtered_data = filtered_data[filtered_data['Start_Lng'] <= north]
+
+    for index, row in filtered_data.iterrows():
+        result.append({"Latitude":row['Start_Lat'],"Longitude":row['Start_Lng'], "Count":row['Count']})
     return result
     
 
--- api/server.py
+++ api/server.py
@@ -24,10 +24,35 @@ def read_root():
         "hello":"test"
     }
 
+# @app.get("/nearby_accidents/")
+# async def nearby_accidents(user_lat: float, user_lng: float):
+#     temp_radius = 5
+#     return points_within_radius(data, (user_lat, user_lng), temp_radius)
+
 @app.get("/nearby_accidents/")
-async def nearby_accidents(user_lat: float, user_lng: float):
-    temp_radius = 5
-    return points_within_radius(data, (user_lat, user_lng), temp_radius)
+async def nearby_accidents(north: float, south: float, east: float, west: float):
+
+    # find the radius
+    y = abs(north - south) * 54.6  # longitude converted to miles
+    x = abs(west-east) * 69        # latitude converted to miles
+    dist_miles = max(x , y) / 2         # get the larger radius in miles
+
+    if dist_miles < 30:
+        # return individual car accidents
+        return points_within_bounds(data, north, south, east, west)
+    
+    elif dist_miles < 50:
+        # return cities (lat,lng) and their car crash counts
+        return grouped_crashes_in_bounds(city_data, north, south, east, west)
+
+    elif dist_miles < 70:
+        # return counties (lat,lng) and their car crash counts
+        return grouped_crashes_in_bounds(county_data, north, south, east, west)
+
+    else:
+        # return states (lat, lng) and their car crash counts
+        return grouped_crashes_in_bounds(state_data, north, south, east, west)
+
 
 if __name__ == "__main__":
     pass
